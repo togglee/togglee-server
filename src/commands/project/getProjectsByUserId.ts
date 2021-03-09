77cc7b4e7db5723eaae856851db9aa399bfeb989
kanekotic <me@kanekotic.com>
2021-03-09 12:11:37 +0100

    Alvaro | fix lint

--- src/commands/project/getProjectsByUserId.ts
+++ src/commands/project/getProjectsByUserId.ts
@@ -2,7 +2,7 @@ import logger from '../../utils/logger';
 import sqlProjectDatabase from '../../dataSources/project/SqlDatabase';
 import { Project } from '../../types/Project';
 
-const baseUrl = "https://togglee-toggles.ams3.digitaloceanspaces.com"
+const baseUrl = 'https://togglee-toggles.ams3.digitaloceanspaces.com';
 // https://togglee-toggles.ams3.digitaloceanspaces.com/869743b0-b854-415b-9caf-81748da8bd31/default.json
 export default async function getProjectsByUserId(
   dataSource: sqlProjectDatabase,
@@ -10,7 +10,10 @@ export default async function getProjectsByUserId(
 ): Promise<Project[]> {
   try {
     const projects = await dataSource.getProjectsByUserId(userId);
-    return projects.map(project => ({...project, url: `${baseUrl}/${project.owner}/${project.name}.json` }))
+    return projects.map((project) => ({
+      ...project,
+      url: `${baseUrl}/${project.owner}/${project.name}.json`,
+    }));
   } catch (error) {
     logger.error(error);
   }
