6ea834e214bd0fe801aca0279ef9ec2aa242e462
kanekotic <me@kanekotic.com>
2021-03-15 09:58:40 +0100

    Alvaro | try activate upload of files

--- src/utils/fileUploader.ts
+++ src/utils/fileUploader.ts
@@ -1,11 +1,12 @@
 // import { Endpoint, S3 } from 'aws-sdk';
+const AWS = require('aws-sdk')
 
-// const spacesEndpoint = new Endpoint('ams3.digitaloceanspaces.com');
-// const s3 = new S3({
-//   endpoint: spacesEndpoint,
-//   accessKeyId: process.env.SPACES_KEY,
-//   secretAccessKey: process.env.SPACES_SECRET,
-// });
+const spacesEndpoint = new AWS.Endpoint('ams3.digitaloceanspaces.com');
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.SPACES_KEY,
+  secretAccessKey: process.env.SPACES_SECRET,
+});
 
 export async function uploadFile(
   name: string,
@@ -13,14 +14,14 @@ export async function uploadFile(
   toggles: any, // eslint-disable-line
   isTest: boolean
 ): Promise<void> {
-  // await s3
-  //   .putObject({
-  //     Bucket: 'togglee-toggles',
-  //     Key: `${owner}/${name}.json`,
-  //     Body: JSON.stringify(toggles),
-  //     ContentType: 'application/json',
-  //     ACL: 'public-read',
-  //     Metadata: { isTest: `${isTest}` },
-  //   })
-  //   .promise();
+  await s3
+    .putObject({
+      Bucket: 'togglee-toggles',
+      Key: `${owner}/${name}.json`,
+      Body: JSON.stringify(toggles),
+      ContentType: 'application/json',
+      ACL: 'public-read',
+      Metadata: { isTest: `${isTest}` },
+    })
+    .promise();
 }
