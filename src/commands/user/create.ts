16d10f59ce488e4574e17d49d18dca8579749b8d
kanekotic <me@kanekotic.com>
2021-03-18 09:48:15 +0100

    Alvaro | change a bit debug lines

--- src/commands/user/create.ts
+++ src/commands/user/create.ts
@@ -9,8 +9,6 @@ export default async function createUser(
   isTestRequest: boolean
 ): Promise<SimpleResponse> {
   try {
-    console.log('creating user');
-    console.log(email);
     await dataSource.create(email, password, isTestRequest);
     return { success: true };
   } catch (error) {
