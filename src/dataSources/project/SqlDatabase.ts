e3fe214fe9ac8008b6d3ca21b029809983142c84
kanekotic <me@kanekotic.com>
2021-03-18 10:54:00 +0100

    Alvaro | insert object directly

--- src/dataSources/project/SqlDatabase.ts
+++ src/dataSources/project/SqlDatabase.ts
@@ -21,14 +21,14 @@ export default class SqlDatabase extends SQLDataSource {
           id,
           name,
           userReference: user,
-          toggles: JSON.stringify(toggles),
+          toggles,
           isTest: isTestRequest ? 1 : 0,
         })
         .into('PROJECTS');
     } catch (error) {
       await this.db('PROJECTS')
         .update({
-          toggles: JSON.stringify(toggles),
+          toggles,
         })
         .where({
           name,
